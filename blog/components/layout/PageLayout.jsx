1cbbecdc9d7a894279670f2e1ab6a790d2eec42f
Matan Shaviro <matan3sh@gmail.com>
2020-11-16 22:35:13 +0200

    Style improvments

--- blog/components/layout/PageLayout.jsx
+++ blog/components/layout/PageLayout.jsx
@@ -1,14 +1,23 @@
 import Header from './Header';
 import Footer from './Footer';
 import { Container } from 'react-bootstrap';
+import Head from 'next/head';
 
 const PageLayout = ({ children, className }) => {
   return (
-    <Container>
-      <Header />
-      <div className={`page-wrapper ${className}`}>{children}</div>
-      <Footer />
-    </Container>
+    <>
+      <Head>
+        <link
+          href='https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;900&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <Container>
+        <Header />
+        <div className={`page-wrapper ${className}`}>{children}</div>
+        <Footer />
+      </Container>
+    </>
   );
 };
 
