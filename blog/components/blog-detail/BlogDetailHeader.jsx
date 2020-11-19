9393b2e975ba45cf1a67fe9791355e4a15bf9c37
Matan Shaviro <matan3sh@gmail.com>
2020-11-19 22:04:03 +0200

    Render blog data in blog detail page

---
+++ blog/components/blog-detail/BlogDetailHeader.jsx
@@ -0,0 +1,25 @@
+const BlogDetailHeader = ({ blog }) => {
+  return (
+    <div className='blog-detail-header'>
+      <p className='lead mb-0'>
+        <img
+          src={blog?.author?.avatar}
+          className='rounded-circle mr-3'
+          height='50px'
+          width='50px'
+          alt='avatar'
+        />
+        {blog?.author?.name}
+        {', '} {blog?.date}
+      </p>
+      <h1 className='font-weight-bold blog-detail-header-title mb-0'>
+        {blog?.title}
+      </h1>
+      <h2 className='blog-detail-header-subtitle mb-3'>{blog?.subtitle}</h2>
+      {/* Check if contains cover image */}
+      <img className='img-fluid rounded' src={blog?.coverImage} alt='' />
+    </div>
+  );
+};
+
+export default BlogDetailHeader;
